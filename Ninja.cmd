0ec193f1018a56fc92a852137462ee6f2a8ad482
illsk1lls <127640867+illsk1lls@users.noreply.github.com>
2023-09-17 16:29:56 -0400

    Update Ninja.cmd
    ocd  ;P

--- Ninja.cmd
+++ Ninja.cmd
@@ -1,7 +1,7 @@
 <# ::
 @ECHO OFF
 >nul 2>&1 REG ADD HKCU\Software\classes\.PSninja\shell\runas\command /f /ve /d "CMD /x /d /r SET \"f0=%%2\"& CALL \"%%2\" %%3"&SET _= %*
->nul 2>&1 FLTMC|| IF "%f0%" NEQ "%~f0" (cd.>"%temp%\runas.PSninja" & START "%~n0" /high "%temp%\runas.PSninja" "%~f0" "%_:"=""%" & EXIT /b)
+>nul 2>&1 FLTMC|| IF "%f0%" NEQ "%~f0" (cd.>"%temp%\runas.PSninja" & START "%~n0" /high "%temp%\runas.PSninja" "%~f0" "%_:"=""%"&EXIT /b)
 >nul 2>&1 REG DELETE HKCU\Software\classes\.PSninja\ /f&>nul 2>&1 DEL %temp%\runas.PSninja /f /q
 (IF EXIST "%temp%\%~n0.ps1" DEL "%temp%\%~n0.ps1" /f /q)&(MKLINK "%temp%\%~n0.ps1" "%~f0">nul)&POWERSHELL -nop -ep bypass -f "%temp%\%~n0.ps1" %*&DEL "%temp%\%~n0.ps1" /f /q&GOTO :EOF
 #>
