688d026ae64fbf640d1c7e453356f94f57b5f2c4
illsk1lls <127640867+illsk1lls@users.noreply.github.com>
2024-02-18 22:49:48 -0500

    Update Ninja.cmd

--- Ninja.cmd
+++ Ninja.cmd
@@ -4,7 +4,8 @@
 >nul 2>&1 FLTMC|| IF "%f0%" NEQ "%~f0" (cd.>"%temp%\runas.PSninja" & START "%~n0" /high "%temp%\runas.PSninja" "%~f0" "%_:"=""%"&EXIT /b)
 >nul 2>&1 REG DELETE HKCU\Software\classes\.PSninja\ /f&>nul 2>&1 DEL %temp%\runas.PSninja /f /q
 (IF EXIST "%temp%\%~n0.ps1" DEL "%temp%\%~n0.ps1" /f /q)&(MKLINK "%temp%\%~n0.ps1" "%~f0">nul)&POWERSHELL -nop -ep bypass -f "%temp%\%~n0.ps1" %*&DEL "%temp%\%~n0.ps1" /f /q&GOTO :EOF
-#> ###POWERSHELL BELOW THIS LINE###
+#>$PSCommandPath=$PSCommandPath.Replace("/\.[^/.]+$/", ".cmd")
+###POWERSHELL BELOW THIS LINE###
 $Url='http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/2000920063/AcroRdrDC2000920063_en_US.exe'
 function Size-To-Human-Readable([uint64]$size){
 	$suffix = "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"
